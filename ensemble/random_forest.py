d9871d80ca4d32d1914f7fe10dca5f78c4b11596
Yannick Bäumer <yannick.baeumer@student.hpi.de>
2017-05-31 10:58:09 +0200

    cleaning up random forest

--- ensemble/random_forest.py
+++ ensemble/random_forest.py
@@ -1,114 +1,30 @@
-from bs4 import BeautifulSoup
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
-import re
 import pandas as pd
-import nltk
-
-from nltk.corpus import stopwords # Import the stop word list
+import preprocessor as pp
 
 class RandomForestClassifier:
     def __init__(self):
         self.model = None
         pass
 
-    def comment_to_words(raw_comment):
-        # Function to convert a raw review to a string of words
-        # The input is a single string (a raw movie review), and
-        # the output is a single string (a preprocessed movie review)
-        #
-        # 1. Remove HTML
-        comment_text = BeautifulSoup(raw_comment).get_text()
-        #
-        # 2. Remove non-letters
-        letters_only = re.sub("[^a-zA-Z]", " ", comment_text)
-        #
-        # 3. Convert to lower case, split into individual words
-        words = letters_only.lower().split()
-        #
-        # 4. In Python, searching a set is much faster than searching
-        #   a list, so convert the stop words to a set
-        stops = set(stopwords.words("german"))
-        #
-        # 5. Remove stop words
-        meaningful_words = [w for w in words if not w in stops]
-        #
-        # 6. Join the words back into one string separated by space,
-        # and return the result.
-        return (" ".join(meaningful_words))
-        # return comment_text
-
-    def createFeatureMatrix(self, df):
-        # Get the number of reviews based on the dataframe column size
-        num_comments = df["comment"].size
-
-        # Initialize an empty list to hold the clean reviews
-        clean_train_comments = []
-
-        # Loop over each review; create an index i that goes from 0 to the length
-        # of the movie review list
-        for i in range(0, num_comments):
-            # Call our function for each one, and add the result to the list of
-            # clean reviews
-            clean_train_comments.append(self.comment_to_words(df["comment"][i]))
-
-            print("Creating the bag of words...\n")
-
-            # Initialize the "CountVectorizer" object, which is scikit-learn's
-            # bag of words tool.
-            vectorizer = CountVectorizer(analyzer="word",
-                                         tokenizer=None,
-                                         preprocessor=None,
-                                         stop_words=None,
-                                         max_features=10000)
-
-            # fit_transform() does two functions: First, it fits the model
-            # and learns the vocabulary; second, it transforms our training data
-            # into feature vectors. The input to fit_transform should be a list of
-            # strings.
-            comment_data_features = vectorizer.fit_transform(clean_train_comments)
-
-            # Numpy arrays are easy to work with, so convert the result to an
-            # array
-            comment_data_features = comment_data_features.toarray()
-
-        return comment_data_features
-
     def fit(self, train_df):
         print("Training the random forest...")
 
-        # Initialize a Random Forest classifier with 100 trees
-        forest = RandomForestClassifier(n_estimators=100)
+        # Initialize a Random Forest classifier
+        forest = RandomForestClassifier()
 
         # Fit the forest to the training set, using the bag of words as
         # features and the sentiment labels as the response variable
         #
         # This may take a few minutes to run
-        trainingFeatures = self.createFeatureMatrix(self, train_df);
+        trainingFeatures = pp.trainFeatureMatrix(train_df);
+        print(trainingFeatures)
         self.model = forest.fit(trainingFeatures, train_df["hate"])
         print("done")
 
 
     def test(self, test_df):
-        # Create an empty list and append the clean reviews one by one
-        num_comments = len(test_df["comment"])
-        clean_test_comments = []
-
-        print("Cleaning and parsing the test set movie reviews...\n")
-        for i in range(0, num_comments):
-            if (i + 1) % 1000 == 0:
-                print("Comment %d of %d\n" % (i + 1, num_comments))
-            clean_comment = self.comment_to_words(test_df["comment"][i])
-            clean_test_comments.append(clean_comment)
-
-        vectorizer = CountVectorizer(analyzer="word",
-                                     tokenizer=None,
-                                     preprocessor=None,
-                                     stop_words=None,
-                                     max_features=10000)
-
-        # Get a bag of words for the test set, and convert to a numpy array
-        test_data_features = vectorizer.transform(clean_test_comments)
+        test_data_features = pp.createFeatureMatrix(test_df)
         test_data_features = test_data_features.toarray()
 
         # Use the random forest to make sentiment label predictions
@@ -129,6 +45,6 @@ class RandomForestClassifier:
 
     def predict(self, comment):
         df = pd.Series([comment])
-        features = self.createFeatureMatrix(self, df)
+        features = pp.createFeatureMatrix(df)
 
         return self.model.predict(features)
--- ensemble/webserver.py
+++ ensemble/webserver.py
@@ -1,5 +1,6 @@
 from bag_of_words import BagOfWordsClassifier
 from text_features import TextFeatureClassifier
+from random_forest import RandomForestClassifier
 import pandas as pd
 import numpy as np
 from flask import *
@@ -10,28 +11,34 @@ class Predictor:
     # self.test_df = pd.read_csv('../../data/tiny.csv', sep=',')
     self.train_df = pd.read_csv('../../data/train.csv', sep=',')
     self.test_df = pd.read_csv('../../data/test.csv', sep=',')
-    self.bag_of_words_classifier = BagOfWordsClassifier()
-    self.bag_of_words_classifier.fit(self.train_df)
-    self.text_features_classifier = TextFeatureClassifier()
-    self.text_features_classifier.fit(self.train_df)
+    # self.bag_of_words_classifier = BagOfWordsClassifier()
+    # self.bag_of_words_classifier.fit(self.train_df)
+    # self.text_features_classifier = TextFeatureClassifier()
+    # self.text_features_classifier.fit(self.train_df)
+    self.random_forest_classifier = RandomForestClassifier()
+    self.random_forest_classifier.fit(self.train_df)
 
   def accuracy(self):
-    bow_accuracy = self.bag_of_words_classifier.test(self.test_df)
-    tf_accuracy = self.text_features_classifier.test(self.test_df)
+    # bow_accuracy = self.bag_of_words_classifier.test(self.test_df)
+    # tf_accuracy = self.text_features_classifier.test(self.test_df)
+    rf_accuracy = self.random_forest_classifier.test(self.test_df)
     return {
-      'bag_of_words': np.asscalar(bow_accuracy),
-      'text_features': np.asscalar(tf_accuracy)
+      # 'bag_of_words': np.asscalar(bow_accuracy),
+      # 'text_features': np.asscalar(tf_accuracy),
+      'random_forest': np.asscalar(rf_accuracy)
     }
 
   def predict(self, comment):
     bow = self.bag_of_words_classifier.predict_with_info(comment)
     tf = self.text_features_classifier.predict(comment)
+    rf = self.random_forest_classifier.predict(comment)
     
     return {
       'comment': comment,
       'bag_of_words': bow["predicted"][0],
       'hate_words': bow["hate_words"],
-      'text_features': tf.tolist()
+      'text_features': tf.tolist(),
+      'random_forest': rf
     }
 
 predictor = Predictor()
@@ -45,8 +52,9 @@ app = Flask(__name__)
 def hello():
   acc = predictor.accuracy()
   data = {
-    'bag_of_words': acc['bag_of_words'],
-    'text_features': acc['text_features']
+    # 'bag_of_words': acc['bag_of_words'],
+    # 'text_features': acc['text_features'],
+    'random_forest': acc['random_forest']
   }
   return jsonify(data)
 
