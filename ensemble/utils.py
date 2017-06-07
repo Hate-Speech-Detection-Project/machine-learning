3552e7185e83196c6d46c86da5dd4ecfdfb58a7e
hater <you@example.com>
2017-06-07 17:35:31 +0200

    added confusion matrix to output

---
+++ ensemble/utils.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class ConfusionMatrix:
+    def __init__(self, predictions, reality):
+        self.predictions = 0
+        self.positives = 0
+        self.negatives = 0
+        self.trueNegatives = 0
+        self.truePositives = 0
+        self.falseNegatives = 0
+        self.falsePositives = 0
+        self.correctPredictions = 0
+
+        for index, prediction in enumerate(predictions) :
+            self.predictions += 1
+            if prediction == 1:
+                self.positives +=1
+                if reality[index] == 1:
+                    self.truePositives += 1
+                else:
+                    self.falsePositives +=1
+            else:
+                self.negatives +=1
+                if reality[index] == 0:
+                    self.trueNegatives += 1
+                else:
+                    self.falseNegatives += 1
+
+        self.correctPredictions = self.truePositives + self.falseNegatives
+
+    # true negative rate
+    def getPrecision(self):
+        return self.trueNegatives/self.negatives
+
+    # true positive rate
+    def getRecall(self):
+        if self.positives == 0:
+            return 0
+        return self.truePositives/self.positives
+
+    def getAccuracy(self):
+        return self.correctPredictions/self.predictions
+
+    def toString(self):
+        return "Accuracy: " + str(self.getAccuracy()) + "/Precision: " + str(self.getPrecision()) + "/Recall: " + str(self.getRecall())
