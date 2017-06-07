6fb31154554775f150d09c9ad97b607770117e5c
Jan Ladleif <jan-ladleif@web.de>
2017-06-07 14:18:47 +0200

    output true/false positives/negatives to files

--- filters/testrunner.py
+++ filters/testrunner.py
@@ -1,11 +1,20 @@
 import psycopg2
 import psycopg2.extras
 import sys
+import os
+import errno
 import requests
 import cProfile
 from filter_manager import FilterManager
 
-FETCH_SIZE = 500000
+FETCH_SIZE = 50000
+
+def make_sure_path_exists(path):
+  try:
+    os.makedirs(path)
+  except OSError as exception:
+    if exception.errno != errno.EEXIST:
+      raise
 
 # Connect to the database.
 try:
@@ -14,6 +23,21 @@ except:
     print("Cannot connect to database")
     sys.exit(0)
 print("Connected to database")
+
+make_sure_path_exists('output')
+tp_file = open('output/tp.csv', 'w')
+fp_file = open('output/fp.csv', 'w')
+tn_file = open('output/tn.csv', 'w')
+fn_file = open('output/fn.csv', 'w')
+tp_file.write('cid,pid,uid,comment,created,url,fid,timestamp,hate\n')
+fp_file.write('cid,pid,uid,comment,created,url,fid,timestamp,hate\n')
+tn_file.write('cid,pid,uid,comment,created,url,fid,timestamp,hate\n')
+fn_file.write('cid,pid,uid,comment,created,url,fid,timestamp,hate\n')
+
+def write_comment(file, comment):
+  order = [str(comment[index]).replace('"', '""')
+           for index in ('cid','pid','uid','comment','created','url','fid','timestamp','hate')]
+  file.write('"' + '","'.join(order) + '"' + '\n')
 
 # Classify the comments.
 def do_stuff():  
@@ -56,13 +80,17 @@ def do_stuff():
         if filter_result['result']:
           if comment['hate']:
             tp += 1
+            write_comment(tp_file, comment)
           else:
             fp += 1
+            write_comment(fp_file, comment)
         else:
           if comment['hate']:
             fn += 1
+            write_comment(tn_file, comment)
           else:
             tn += 1
+            write_comment(fn_file, comment)
       
       if total%5000 == 0:
         print("%i,%i,%i,%i,%i,%i" % (total, filtered, tp, fp, tn, fn))
