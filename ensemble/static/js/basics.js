cc2de3d62434c35f775d44d7f7e63f91c3dd5ede
Tim Naumann <tim.naumann@student.hpi.uni-potsdam.de>
2017-07-08 22:23:54 +0200

    added api-url

--- ensemble/static/js/basics.js
+++ ensemble/static/js/basics.js
@@ -1,9 +1,10 @@
 function submitComment() {
     const comment = document.getElementById("comment").value;
     const url = document.getElementById("url").value;
+    const apiURL = '//localhost:5000/predict/commentid';
 
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "POST", url, false );
+    xmlHttp.open( "POST", apiURL, false );
 
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
