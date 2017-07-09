9b77696ab56a9e3272d30c69e1ece507def7d092
Tim Naumann <tim.naumann@student.hpi.uni-potsdam.de>
2017-07-09 20:09:51 +0200

    fixed issue with javascript request

--- ensemble/static/js/basics.js
+++ ensemble/static/js/basics.js
@@ -1,17 +1,18 @@
 function submitComment() {
     const comment = document.getElementById("comment").value;
     const url = document.getElementById("url").value;
-    const apiURL = '//localhost:5000/predict';
+    const apiURL = '//127.0.0.1:5000/predict';
 
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "POST", apiURL, false );
 
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
-           console.log("fine");
+           alert(JSON.stringify(JSON.parse(xmlHttp.responseText)))
         }
     }
     xmlHttp.setRequestHeader('Content-type', 'application/json');
+    xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
 
     const payload = {
             'comment': comment,
